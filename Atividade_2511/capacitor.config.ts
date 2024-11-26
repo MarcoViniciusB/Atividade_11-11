f6b7ca4f0e20d81f0cb8e8c155b518b726cc1a0c
MarcoViniciusB <mvbpmiab@gmail.com>
2024-11-25 21:52:47 -0300

    Atividade_APS

---
+++ Atividade_2511/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Atividade_2511',
+  webDir: 'www'
+};
+
+export default config;
